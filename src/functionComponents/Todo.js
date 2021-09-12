a0e5fa28c9520a21869b63e255569daf9558aee2
Austin <austinow@gmail.com>
2021-09-12 11:11:15 -0700

    edit has map issue

--- src/functionComponents/Todo.js
+++ src/functionComponents/Todo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Todo({ todo, todos, toggleTodo, setTodos }) {
+export default function Todo({ todo, todos, toggleTodo, setTodos, todoEditing, setTodoEditing, editingText, setEditingText }) {
     function handleTodoClick(){
         toggleTodo(todo.id)
     }
@@ -10,6 +10,18 @@ export default function Todo({ todo, todos, toggleTodo, setTodos }) {
         const updatedTodo = [...todos].filter((todo) => todo.id !== id);
         setTodos(updatedTodo)
     }
+
+    function editTodo(id){
+        const updatedTodos = [...todos].map((todo) =>{
+            if(todo.id === id){
+                todo.text = editingText
+            }
+            return todo
+        })
+        setTodos(updatedTodos)
+        setTodoEditing(null)
+        setEditingText('')
+    };
 
     return (
         <div>
@@ -21,6 +33,17 @@ export default function Todo({ todo, todos, toggleTodo, setTodos }) {
                 />
                 {todo.name}
                 <button onClick = {() => deleteTodo(todo.id) }>delete</button>
+                <button onClick={() => setTodoEditing(todo.id)}>edit</button>
+                <button onClick = {() => editTodo(todo.id)}>Submit Edit</button>
+
+                {todoEditing === todo.id
+                ? 
+                (<input type='text' 
+                        onChange={(e) => setTodos(e.target.value)} 
+                        value={editingText}/>) 
+                            : (
+                                <div>{todo.text}</div>
+                            )}
             </label>
             
         </div>
