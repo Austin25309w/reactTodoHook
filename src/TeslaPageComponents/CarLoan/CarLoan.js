9394d71cc0fa811fb35223c56a9efedb30268699
Austin <austinow@gmail.com>
2022-01-11 23:43:20 -0800

    added functionality in carloan calculator

--- src/TeslaPageComponents/CarLoan/CarLoan.js
+++ src/TeslaPageComponents/CarLoan/CarLoan.js
@@ -1,30 +1,57 @@
-import React from 'react';
+import {React, useState} from 'react';
 import './carLoan.css'
 
 const CarLoan = () => {
+    const[amount, setAmount] = useState(0)
+    const[interest, setInterest] = useState(0)
+    const[period, setPeriod] = useState(0)
+    const[ans, setAns] = useState()
+
+    function loanCalculation(){
+            let decimalRate = interest / 100;
+            let monthRate = decimalRate / 12;
+            let amountInterest = amount * monthRate;
+            let plusOne = monthRate + 1;
+            let byPeriod = Math.pow(plusOne, period);
+            let byOne = 1 / byPeriod ;
+            let oneSubtract = 1 - byOne;
+            let finalAns = Math.round(amountInterest) / oneSubtract.toFixed(2) 
+            setAns(finalAns);
+    }
+
+    function addComma(x){
+        let comma = x.toString().split(".");
+        comma[0] = comma[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return comma.join('.') 
+    }
+
+    // 6.74%, 36mo, $10,000 / 311.11
+
     return (<div className='car_loan'>
         <h1>CarLoan</h1>
-        <div>
-        <p>Loan Amount</p>
-        <input></input>
-        </div>
-        <div>
-        <p>Interest rate (%)</p>
-        <input></input>
-       </div>
-       <div>
-        <p>Loan Period (months)</p>
-        <input></input>
-       </div>
+        <form >
+            <div>
+                <p >Loan Amount</p>
+                <input type='number' placeholder='amount' onChange={ e => setAmount(e.target.value)}/>
+            </div>
+            <div>
+                <p>Interest rate (%)</p>
+                <input type='number' placeholder='rate' onChange={ e => setInterest(e.target.value)}/>
+            </div>
+            <div>
+                <p>Loan Period (months)</p>
+                <input type='number' placeholder='month' onChange={ e => setPeriod(e.target.value)}/>
+            </div>
+        </form>
+        
 
         <div>
             <h4>Total cost of car loan</h4>
-            {/* <h4>{this.state.carLoan}</h4> */}
         </div>
 
         <div>
-            <h4>Monthly Payments</h4>
-            {/* <h4>{this.state.monthlyPayments}</h4> */}
+            <button onClick={ () =>loanCalculation(amount, interest, period)}>submit </button>
+            <span><h4>Monthly Payments</h4>${ans}</span>
         </div>
         
     </div>)
@@ -32,6 +59,7 @@ const CarLoan = () => {
 }
 
 export default CarLoan
+
 
 // 1. divide interest rate by 100 # 3.11 x 100 = 0.0311
 // 2. divide the interest rate by 12  # 0.0311 / 12 = 0.0025
@@ -41,4 +69,3 @@ export default CarLoan
 // 6. use 1 to divide 1.16 = 0.86
 // 7. subtract this number from 1 # 1 - 0.86 = 0.14
 // 8. divide the number step to the number step 7 # 62.5 / 0.14 = 446.42
-
