a61b6c3dd5881b1d782db812c8582c07482e010b
Austin <austinow@gmail.com>
2022-01-25 12:06:43 -0800

    fixed many responsive css

--- src/TeslaPageComponents/Stocks/StocksComponent.js
+++ src/TeslaPageComponents/Stocks/StocksComponent.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './StocksComponent.css'
 
 export default function StocksComponent() {
     return (
         <div>
             <h1>Stocks</h1>
-            <img width='500px' src ="https://knowtechie.com/wp-content/uploads/2021/03/dogecoin-meme.jpg.webp" />
+            <img className='stockImage' src ="https://knowtechie.com/wp-content/uploads/2021/03/dogecoin-meme.jpg.webp" />
         </div>
     )
 }
