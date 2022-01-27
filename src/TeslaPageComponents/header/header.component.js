2001b76341c6d1fb48dbea81cb475067bcdb466b
Austin <austinow@gmail.com>
2022-01-26 16:26:07 -0800

    moved the menu to dropup, issues between desktop and mobile, still have issue with CALCULATION inner tab

--- src/TeslaPageComponents/header/header.component.js
+++ src/TeslaPageComponents/header/header.component.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import './header.styles.css'
 import Time from '../Time/Time';
@@ -6,40 +6,64 @@ import Resizewindow from '../../functionComponents/resizeWindowWIdth/Resizewindo
 import MiniWeather from '../Weather/MiniWeather';
 
 
-const Header = () => (
-    <div className='header_container'>
+const Header = () => {
+    const [calToggle, setCalToggle] = useState(false);
+    const [menuToggle, setMenuToggle] = useState(false);
 
-        <div className='headerA'>
-                <Link to='/reactTodoHook/' className='option'>HOME </Link>
-                    <div className='dropup'>
-                        <Link className="option" >
+
+    const toggleCalculation = () => {
+        setCalToggle(!calToggle)
+        console.log('1')
+    }
     
-                        <div className='dropup-content'>
-                            <Link to='/reactTodoHook/calculator'>Calculator</Link>
-                            <Link to='/reactTodoHook/aprcalculator'>APR Cal</Link>
-                            <Link to='/reactTodoHook/carloancalculator' >Car Loan</Link>
-                            <Link to='/reactTodoHook/evmiles'>EV miles</Link>
+
+    const toggleMenu =() => {
+        setMenuToggle(!menuToggle);
+    }
+
+
+
+ 
+
+    return (
+    
+        <div className='header_container'>
+            {/* { menuToggle? 'headerA': 'headerA-hide'} */}
+            <div className= {window.innerWidth < 450 ? menuToggle? 'headerA': 'headerA-hide' : 'headerA'}>
+                    <Link onClick={toggleMenu} to='/reactTodoHook/' className='option'>HOME </Link>
+                        <div className='dropup'>
+                            <Link onClick={toggleCalculation}  className="option" >
+        
+                            <div  className={calToggle? 'dropup-content': 'dropup-content-off'}>
+                                <Link  to='/reactTodoHook/calculator'>Calculator</Link>
+                                <Link  to='/reactTodoHook/aprcalculator'>APR Cal</Link>
+                                <Link  to='/reactTodoHook/carloancalculator' >Car Loan</Link>
+                                <Link  to='/reactTodoHook/evmiles'>EV miles</Link>
+                            </div>
+    
+                                CALCULATION </Link>
                         </div>
-
-                            CALCULATION </Link>
-                    </div>
-                <Link className='option' to='/reactTodoHook/todo'>
-                    TODO </Link>
-                    <Link className='option' to='/reactTodoHook/stocks'>
-                STOCKS </Link>
-                <Link className='option' to='/reactTodoHook/expensemanager'>
-                G-SUITE </Link>
-        </div>
-
-        <div className='bottomTime'>
-            <span><Time/></span>
-        </div>  
-        <div className='bottomWeather'>
-        <span><MiniWeather/> </span>
-        </div>
-        <div className='resizeWindow'><Resizewindow/></div>
-    </div>
+                    <Link onClick={toggleMenu} className='option' to='/reactTodoHook/todo'>
+                        TODO </Link>
+                        <Link onClick={toggleMenu} className='option' to='/reactTodoHook/stocks'>
+                    STOCKS </Link>
+                    <Link onClick={toggleMenu} className='option' to='/reactTodoHook/expensemanager'>
+                    G-SUITE </Link>
+            </div>
     
-)
+            <div className='bottomTime'>
+                <span><Time/></span>
+            </div>  
+            <div className='bottomWeather'>
+            <span><MiniWeather/> </span>
+            </div>
+            <div className='resizeWindow'><Resizewindow/></div>
+            <button className='hamburgerMenu' onClick={toggleMenu}>Menu</button>
+        </div>
+        
+    )
+}
+
+
 
 export default Header
---
+++ src/functionComponents/Tools/Tools.js
@@ -0,0 +1,7 @@
+// window.scroll({
+//     top: 0,
+//     left: 0, 
+//     behavior: 'smooth',
+//   });
+
+// to scroll all the way up 
