f17c361dddd626b85d10920d71b35eb3e42dbda9
Austin <austinow@gmail.com>
2022-02-15 11:46:44 -0800

    Updates

---
+++ SNAKE/game.js
@@ -0,0 +1,42 @@
+import { SNAKE_SPEED, update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection } from './snake.js'
+import {update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
+
+let lastRenderTime = 10
+let gameOver = false;
+
+const gameBoard = document.getElementById('game-board')
+
+function main(currentTime){
+    if(gameOver) {
+        if(confirm('You lost. Press OK to restart')){
+            window.location = 'http://127.0.0.1:5500/public/SNAKE/snake.html'
+        }
+        return
+    }
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if(secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+    lastRenderTime = currentTime
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+ 
+function update() {
+    updateSnake()
+    updateFood()
+    checkDeath()
+}
+function draw(){ 
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath(){
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
